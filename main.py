58dae0ce6d50d1bd544a36ad5fef6c565f29b327
I_Ayobi18 <I_Ayobi18@cranford.hounslow.sch.uk>
2024-03-18 13:40:37 +0000

    converter hard code

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print ("hello world")
-name = input("Enter your name")
-print("hello" , name)
+import streamlit as st
+
+st. title("Currency Converter")
+st.text_input("Enter amount:")
+st.radio("Select currency:", ["GBP to USD", "GBP to JPY", "GBP to EUR"])
+
+col1, col2 = st.columns([1,1])
+
+with col1:
+    st.text("Converted amount:")
+with col2:
+    st.text("900")
+
+
